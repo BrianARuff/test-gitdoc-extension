548c80663cb52c1ac469fe55de3bbdd62e16681d
Brian Ruff <brff19@gmail.com>
2024-10-15 12:09:18 +0000

    Oct 15, 2024, 8:09 AM

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Combobox, ComboxBoxOption } from "./ComboBox";
 
 const options = [
-  "Choose a Fruit",
+  "Choose a Fruit Choose a Fruit Choose a Fruit Choose a Fruit",
   "Apple",
   "Banana",
   "Blueberry",
