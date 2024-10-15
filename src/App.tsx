6c4a48f6a4a9d717262725b3aeb784ad1f8ad5a2
Brian Ruff <brff19@gmail.com>
2024-10-15 00:04:02 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { Combobox, ComboxBoxOption } from "./ComboBox";
+
+const options = [
+  "Choose a Fruit",
+  "Apple",
+  "Banana",
+  "Blueberry",
+  "Boysenberry",
+  "Cherry",
+  "Cranberry",
+  "Durian",
+  "Eggplant",
+  "Fig",
+  "Grape",
+  "Guava",
+  "Huckleberry",
+];
+
+function App() {
+  const formattedOptions: ComboxBoxOption[] = options?.map<ComboxBoxOption>(
+    (option, index): ComboxBoxOption => {
+      const formattedOption = {
+        label: option,
+        value: options + "__" + index,
+        index: index,
+      };
+
+      return formattedOption;
+    }
+  );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        minHeight: "100vh",
+        width: "100%",
+        maxWidth: "100dvw",
+      }}
+    >
+      <Combobox
+        renderLabelSpan={() => {
+          return "Favorite Fruit";
+        }}
+        options={formattedOptions}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ComboBox.tsx
@@ -0,0 +1,141 @@
+import {
+  Dispatch,
+  ReactNode,
+  RefObject,
+  SetStateAction,
+  useCallback,
+  useId,
+  useRef,
+  useState,
+} from "react";
+
+export type ComboxBoxOption = {
+  label: string;
+  value: string;
+  index: number;
+  isSelected?: boolean;
+};
+
+export type RenderLabelSpanProps = {
+  comboboxRef?: RefObject<HTMLDivElement>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  selected: ComboxBoxOption;
+  setSelected: Dispatch<SetStateAction<ComboxBoxOption>>;
+};
+
+export type RenderListOptionProps = {
+  comboboxRef?: RefObject<HTMLDivElement>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  selected: ComboxBoxOption;
+  setSelected: Dispatch<SetStateAction<ComboxBoxOption>>;
+  option: ComboxBoxOption;
+};
+
+export type ComboboxProps = {
+  options?: ComboxBoxOption[];
+  renderLabelSpan?: (props: RenderLabelSpanProps) => ReactNode;
+  renderListOption?: (props: RenderListOptionProps) => ReactNode;
+};
+
+export const Combobox = ({
+  renderLabelSpan,
+  renderListOption,
+  options = [],
+}: ComboboxProps) => {
+  const comboboxRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState<{
+    value: string;
+    label: string;
+    index: number;
+  }>(options?.[0] || {});
+
+  const uid = useId();
+  const comboboxId = `combobox__${uid}`;
+  const comboboxLabelId = `combobox-label__${uid}`;
+
+  const RenderLabelSpan = useCallback(
+    () =>
+      renderLabelSpan?.({
+        comboboxRef,
+        isOpen,
+        selected,
+        setIsOpen,
+        setSelected,
+      }),
+    [isOpen, selected, setIsOpen, setSelected, renderLabelSpan]
+  );
+
+  const RenderListOptions = useCallback(() => {
+    const optionsRenderContent = options?.map((option, index) => {
+      const optionId = `combobox-option-${index}`;
+
+      return (
+        <div
+          className="combobox-option-container"
+          role="option"
+          id={optionId}
+          key={optionId}
+          aria-selected={false}
+        >
+          {renderListOption?.({
+            comboboxRef,
+            isOpen,
+            selected,
+            setIsOpen,
+            setSelected,
+            option,
+          })}
+        </div>
+      );
+    });
+
+    return optionsRenderContent;
+  }, [isOpen, selected, options, setIsOpen, setSelected, renderListOption]);
+
+  return (
+    <div className="combobox-wrapper">
+      <label htmlFor={comboboxId} className={"combobox-label"}>
+        <RenderLabelSpan />
+      </label>
+      <div
+        ref={comboboxRef}
+        id={comboboxId}
+        aria-controls={comboboxId}
+        aria-expanded={isOpen}
+        aria-haspopup={"listbox"}
+        aria-labelledby={comboboxLabelId}
+        role="combobox"
+        className="combobox-container"
+        tabIndex={0}
+        onPointerDown={() => {
+          setIsOpen((prev) => !prev);
+        }}
+        onKeyDown={(e) => {
+          if (
+            e.key === "Enter" ||
+            e.key === " " ||
+            e.key === "ArrowDown" ||
+            e.key === "ArrowUp"
+          ) {
+            setIsOpen((prev) => !prev);
+          }
+        }}
+      />
+      <div
+        id="listbox1"
+        tabIndex={-1}
+        role="listbox"
+        aria-labelledby={comboboxLabelId}
+        className={isOpen ? "show" : "hide"}
+      >
+        <div role="option" id={`combobox-option-index`} aria-selected={false}>
+          <RenderListOptions />
+        </div>
+      </div>
+    </div>
+  );
+};
